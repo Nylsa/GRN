86df37efd2ae26432cf863beb3ce8bab23b2b8bd
Nylsa <nylsa.chammartin@gmail.com>
2024-10-24 10:26:48 +0200

    Modified/Reduced the nb of genes in the list of Step1b python script (85 genes).

--- Step1b_extractgenesfromcombinedtffile.py
+++ Step1b_extractgenesfromcombinedtffile.py
@@ -12,19 +12,19 @@ geneofinterest = [
     'Rela', 'Il6', 'Il8', 'Il1', 'Tnf', 'Cxcl10', 'Tgfa', 'Tgfb', 
     'Stat3', 'Stat6', 'Nlrp1b', 'Nlrp5', 'Nlrp3', 'Nlrp12', 'Nlrp10',
     'Il18', 'Tp53', 'Bax', 'Bcl10',
-    'Bcl2', 'Bcl2a1', 'Bcl2l1', 'Bcl2l10', 'Bcl2l11', 'Bcl2l2', 'Casp1', 'Casp14', 'Casp2', 'Casp3', 'Casp4', 'Casp5', 
+    'Bcl2', 'Casp1', 'Casp14', 'Casp2', 'Casp3', 'Casp4', 'Casp5', 
     'Casp6', 'Casp7', 'Casp8', 'Casp9', 'Max', 'Mcl1', 'Myc', 'Mycn', 'Ndrg1', 'Ndrg2', 'Ndrg3', 'Ndrg4', 'Nlrp1', 
     'Nod1', 'Nol3', 'Ripk1', 'Ripk2', 'Ripk3', 'Rubcn', 
-    'Serpinb5', 'Sfn', 'Sh3glb1', 'Sirt1', 'Siva1', 'Slk', 'Smpd2', 'Sptan1', 'Stk17b', 
-    'Stk24', 'Stk3', 'Stk4', 'Tank', 'Tax1bp1', 'Tnf', 'Tnfaip3', 'Tnfrsf10a', 'Tnfrsf10b', 
-    'Tnfrsf10c', 'Tnfrsf10d', 'Tnfrsf11a', 'Tnfrsf12a', 'Tnfrsf1a', 'Tnfrsf1b', 
-    'Tnfrsf21', 'Tnfrsf25', 'Tnfrsf6b', 'Tnfsf10', 'Tnfsf11', 'Tnfsf12'
+    'Serpinb5', 'Sfn', 'Sh3glb1', 'Siva1', 'Stk17b', 
+    'Stk24', 'Stk3', 'Stk4', 'Tank', 'Tax1bp1', 'Tnf'
 ]    
+
+print(len(geneofinterest))
 
 # Filter the dataframe to only include rows where 'target_genesymbol' or 'source_genesymbol' is in the list of genes of interest
 filtered_df = df[(df['target_genesymbol'].isin(geneofinterest)) | (df['source_genesymbol'].isin(geneofinterest))]
 
 # Save the filtered data to a new file
-filtered_df.to_csv('/home/nylsachammartin/Documents/projects/GeneRegNetworks/GRN/outputdata/Step1b_filtered_genes_either_nylsa_reduced.csv', index=False)
+filtered_df.to_csv('/home/nylsachammartin/Documents/projects/GeneRegNetworks/GRN/outputdata/Step1b_filtered_genes_either_nylsa_reduced_b.csv', index=False)
 
 print("Filtered data saved.")
